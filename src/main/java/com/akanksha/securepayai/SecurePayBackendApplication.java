7256dc6450e7da4f4a491e76021754578715107a
Akanksha <akanksha5064483@gmail.com>
2026-07-03 18:31:30 +0530

    rename issue fixed

--- src/main/java/com/akanksha/securepayai/SecurePayBackendApplication.java
+++ src/main/java/com/akanksha/securepayai/SecurePayBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurepayBackendApplication {
+public class SecurePayBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurepayBackendApplication.class, args);
+		SpringApplication.run(SecurePayBackendApplication.class, args);
 	}
 
 }
