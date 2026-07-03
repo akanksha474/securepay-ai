3ae0dea5bdaa7854f511798383e95b0e1a3a37cf
Akanksha <akanksha5064483@gmail.com>
2026-07-03 18:27:54 +0530

    Initialized SecurePay AI project

---
+++ src/main/java/com/akanksha/securepayai/SecurePayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.akanksha.securepayai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurepayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurepayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akanksha/securepayai/SecurepayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akanksha.securepayai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurepayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
