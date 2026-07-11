0de5343ad67d347ba1b8dee71febacb18aee4595
Akanksha <akanksha5064483@gmail.com>
2026-07-11 12:04:12 +0530

    Implemented Deposit API

--- src/main/java/com/akanksha/securepayai/dto/AccountRequest.java
+++ src/main/java/com/akanksha/securepayai/dto/AccountRequest.java
@@ -1,7 +1,6 @@
 package com.akanksha.securepayai.dto;
 
 import com.akanksha.securepayai.enums.AccountType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
